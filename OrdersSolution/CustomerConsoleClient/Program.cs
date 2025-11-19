fd25e5ecb111aa191e8e4fe67826f103d1e625e7
kannan <kannans89@hotmail.com>
2025-11-19 15:07:23 +0530

    orders

--- OrdersSolution/CustomerConsoleClient/Program.cs
+++ OrdersSolution/CustomerConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CustomoerOrderLib;
+using CustomoerOrderLib.Domain;
 
 namespace CustomerConsoleClient
 {
@@ -6,7 +7,39 @@ namespace CustomerConsoleClient
     {
         static void Main(string[] args)
         {
-            
+
+            var customerHemang = new Customer("C1001", "Hemang");
+            var orderOfDecember = new Order( id: "Ord1", name: "DecemberOrder",date:DateTime.Now);
+            var laptopItem = new OrderItem("bakset1", "Laptop", 20000, 2);
+            var smartvItem = new OrderItem("basket2", "Smart Tv", 50000, 1);
+            orderOfDecember.AddOrderItem(laptopItem);
+            orderOfDecember.AddOrderItem(smartvItem);
+            customerHemang.AddOrder(orderOfDecember);
+            PrintInfo(customerHemang);
+
+
+        }
+
+        private static void PrintInfo(Customer customer)
+        {
+            Console.WriteLine($"id {customer.Id}  and name {customer.Name}");
+            Console.WriteLine($"No of orders placed {customer.Orders.Count} ");
+            var orders= customer.Orders;
+            foreach (Order order in orders)
+            {
+               
+                var basketItems = order.GetOrderItems();
+
+                foreach (OrderItem item in basketItems)
+                {
+                    Console.WriteLine($"item {item.Id} , {item.Name} ,{item.CalcuateTotal()}");
+                }
+
+                Console.WriteLine($"{order.Id} , {order.Name} ,{order.CheckOutPrice()}");
+              
+
+            }
+
         }
     }
 }
